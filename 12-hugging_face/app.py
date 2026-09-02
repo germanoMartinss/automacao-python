4a5d3ed0a73b9a6324ff51cb0778a206aab10f25
Germano Martins <germanopmartins91@gmail.com>
2026-09-02 15:42:03 -0300

    feat: gradio

---
+++ 12-hugging_face/app.py
@@ -0,0 +1,21 @@
+import gradio as gr
+from transformers import pipeline
+from PIL import Image
+
+def remove_background(image):
+    pipeline_model = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
+    pillow_mask = pipeline_model(image, return_mask=True)
+    pillow_image = pipeline_model(image)
+    return pillow_image
+
+# remove_background("img/fefe.png")
+
+app = gr.Interface(
+    title="Remove Background",
+    description="Faça upload de uma imagem e remova o fundo",
+    fn=remove_background,
+    inputs=gr.components.Image(type="pil"),
+    outputs=gr.components.Image(type="pil", format="png"),
+)
+
+app.launch(share=True)
