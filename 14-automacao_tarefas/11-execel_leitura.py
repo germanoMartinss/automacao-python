6f8bf60f939ba4569ee7d0dc1d4e4ac95a5ac8cf
Germano Martins <germanopmartins91@gmail.com>
2026-09-24 14:37:51 -0300

    leitura utilizando o for

--- 14-automacao_tarefas/11-execel_leitura.py
+++ 14-automacao_tarefas/11-execel_leitura.py
@@ -1,6 +1,17 @@
 from openpyxl import load_workbook
 
+# 1 - Lendo planilha Excel
 arquivo = 'files/teste.xlsx'
 wb = load_workbook(arquivo)
-ws = wb.active
-print(ws['A1'].value)
+planilha1 = wb['Planilha 1']
+
+# 2 - Acessando um determinado valor
+# print(planilha1['C2'].value)
+
+# 3 - Interando valores por meio de loop
+for i in range(2, 5):
+    ano = planilha1['A%s' %i].value
+    lucro = planilha1['B%s' %i].value
+    custo = planilha1['C%s' %i].value
+
+    print("{0} teve {1} de lucro e {2} de custos".format(ano, lucro, custo))
